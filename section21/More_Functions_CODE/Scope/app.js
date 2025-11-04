0fe82da7a554ad9729e6b8c1c8cdc9f8a1250130
yamaguch-i <give.mt.mo@gmail.com>
2025-11-04 17:20:34 +0900

    section21

---
+++ section21/More_Functions_CODE/Scope/app.js
@@ -0,0 +1,125 @@
+// let total = 0;
+// function collect() {
+//     total = 6;
+// }
+// console.log(total);
+// collect();
+// console.log(total);
+
+// let bird = 'アオサギ';
+
+// function birdWatch() {
+//     // let bird = 'ムクドリ';
+//     console.log(bird);
+// }
+
+// birdWatch();
+// console.log(bird);
+
+// let radius = 8;
+// if (radius > 0) {
+//     const PI = 3.14;
+//     let msg = 'hogehoge';
+//     console.log(PI);
+// }
+
+// console.log(PI);
+// console.log(msg);
+
+// for (let i = 0; i < 5; i++) {
+//     let msg = 'lskdfjasldfkj';
+// }
+
+// console.log(msg);
+// console.log(i);
+
+// function bankRobbery() {
+//     const heroes = ['スパイダーマン', 'スーパーマン', 'ブラックパンサー'];
+//     function help() {
+//         for (let hero of heroes) {
+//             console.log(`助けて、${hero}!!!`);
+//         }
+//     }
+
+//     help();
+// }
+
+// let x = 'あいうえお';
+// function hoge() {
+//     console.log(x);
+// }
+// function moge() {
+//     let x = 'かきくけこ';
+//     hoge();
+// }
+
+// function add(x, y) {
+//     return x + y;
+// }
+
+// const add = function (x, y) {
+//     return x + y;
+// }
+
+// const PI = 3.14;
+
+// let total = 0;
+// function collect() {
+//     total = 6;
+// }
+// console.log(total);
+
+// collect()
+// console.log(total);
+
+// let bird = 'アオサギ';
+
+// function birdwatch() {
+//     // let bird = 'ムクドリ';
+//     console.log(bird);
+// } 
+// birdwatch()
+// // console.log(bird);
+
+// let radius = 8;
+// if (radius > 0) {
+//     const PI = 3.14;
+//     let msg = 'hogehoge';
+// }
+
+// // console.log(PI);
+// console.log(msg);
+
+// for (var i = 0; i < 5; i++) {
+//     var msg = 'hello';
+// }
+
+// console.log(msg);
+// console.log(i);
+
+// function bankRobbery() {
+//     const heroes = ['スパイダーマン', 'スーパーマン', 'ブラックパンサー'];
+//     function help () {
+//         for (let hero of heroes) {
+//             console.log(`助けて、${hero}!!!`);
+//         }
+//     }
+//     help();
+// }
+
+// let x = 'あいうえお';
+// function hoge() {
+//     console.log(x);
+// }
+// function moge() {
+//     let x = 'かきくけこ';
+//     hoge();
+// }
+
+// function add(x, y) {
+//     return x + y;
+// }
+
+const add = function (x, y) {
+    return x + y;
+}
