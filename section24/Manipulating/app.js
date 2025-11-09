0cd314d4be9177b3bd5890b2e8de77d70f3c9c37
yamaguch-i <give.mt.mo@gmail.com>
2025-11-09 16:42:27 +0900

    section24

---
+++ section24/Manipulating/app.js
@@ -0,0 +1,22 @@
+// const allLinks = document.querySelectorAll('a');
+
+// // for (let link of allLinks) {
+// //     link.innerText = '私はリンクです！！！！';
+// // }
+
+// for (let link of allLinks) {
+//     link.style.color = 'red';
+//     link.style.textDecorationColor = 'magenta';
+//     link.style.textDecorationStyle = 'wavy';
+// }
+
+const allLinks = document.querySelectorAll('a');
+
+// for (let link of allLinks) {
+//     link.innerText = '私はリンクです！！！！'
+// }
+
+for (let link of allLinks) {
+    link.style.color ='red';
+    link.style.textDecorationStyle = 'wavy';
+}
